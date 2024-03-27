8b1a9c093e22ec4c032cc0f1a10a716c2cad5e28
ehteshamsheikh01 <sheikhehtesham301@gmail.com>
2024-03-27 11:27:39 +0500

    Loops Ass

---
+++ app.js
@@ -0,0 +1,74 @@
+
+// Q1)write a programe to display the massage "hello word" 5 Times in your browser suing loops?
+// Ans:
+// for (let i = 1;  i <= 5; i++){
+//     console.log('hello word' ,i);
+//     console.log('loop k bhr')
+// }
+
+
+// Q2) write a program to print numeric counting from 1 to 10?
+// ans:1 2 3 4 5 6 7 8 9 10
+// for (let i = 1;  i <= 10; i++){
+//     console.log('hello word' ,i);
+    // }
+
+    // Q3) write a programe to print multiplication table of any Number
+    // using for loop. table number & lenght should be taken as an 
+    // input from user?
+    // Ans3/  document.write
+
+// var num1 = +prompt('enter value 1')
+// var num2 = +prompt('enter value 2')
+// var add= num1 + num2
+
+//     document.write(
+//  "<table border = '1px'>"+
+//  "<tr>"+
+//  "<th>"+"MULTIPLICATION"+"</th>"
+//  +"</tr>"+
+//  "<tr>"+
+//  "<td>"+MULTIPLY+"</td>"
+//  +"</tr>"
+//  +"</table>"
+//     )
+
+// Q4)You have an array
+// A = [“Nokia”, “Samsung”, “Apple”, “Sony”, “Huawei”]
+// Write each element on new line with the help of for loops
+// Ans:  
+// let A =['Nokia' , 'Samsung' , 'Apple' , 'Sony' , 'Huawei']
+//  console.log(A);
+//  const ul = document.querySelector ('ul');
+    //  for (let i = 0; i < A . length; i++ ){
+    //     console.log(A[i]);
+    //     ul.innerHTML += <li>a[i]</li>
+//  }
+// Q5) write a program to print items of 
+    // the following array using for loops.
+    // apple      element at index 0 is apple
+    // banana     element at index 1 is banana  
+    // mango      element at index 2 is mango
+    // orange     element at index 3 is orange
+    // strawberry element at index 4 is strawberry   
+    
+    // let Elementatindex =['apple' , 'banana' , 'mango' , 'orange' , 'strawberry']
+    // console.log(Elementatindex);
+    // const or = document.querySelector('or')
+    // for (let i = 0; i < Elementatindex . length; i++){
+    //    console.log(Elementatindex[i]);
+    //     or.innerHTML += <li>Elementatindex[i]</li>
+    // }
+    
+    // Q6) write a program to itialize an Array of 
+        // N item by using prompt. where N is number
+        // of items as entered by the User.
+        // Ans: 
+    // const userinput = prompt('enter any number');
+    // const arr =[]
+    // for (let i = 0; i < userinput; i++){
+    //     const arryitem = prompt("enter item you want to push an arry")
+    //     arr.push(arryitem)
+        
+    // }    
+    // console.log(arr)
